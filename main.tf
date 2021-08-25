7adf0a1171b703b32f733a4399149d329f8c5238
Yedukondalu-Yanam <89455823+Yedukondalu-Yanam@users.noreply.github.com>
2021-08-25 18:43:33 +0530

    Update main.tf
    added region

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      region = "us-east-1"
+     
     }
     random = {
       source = "hashicorp/random"
@@ -20,9 +20,9 @@ terraform {
 }
 
 resource "aws_instance" "myec2" {
-    instance_type = "t2.micro"
-    ami = "ami-0c2b8ca1dad447f8a"
-  
+    instance_type  = "t2.micro"
+    ami            = "ami-0c2b8ca1dad447f8a"
+    region         = "us-east-1"
     tags = {
         Name = "Myinstance-Remote-State"
     }
