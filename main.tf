d2e0ba10581fa47fc616d79a962e65f6187ee1b8
Yedukondalu-Yanam <89455823+Yedukondalu-Yanam@users.noreply.github.com>
2021-08-25 18:49:41 +0530

    Update main.tf
    added provider block in terraform block

--- main.tf
+++ main.tf
@@ -7,6 +7,9 @@ terraform {
     random = {
       source = "hashicorp/random"
     }
+    provider "aws" {
+       region         = "us-east-1"
+    }
   }
 
   backend "remote" {
@@ -22,7 +25,7 @@ terraform {
 resource "aws_instance" "myec2" {
     instance_type  = "t2.micro"
     ami            = "ami-0c2b8ca1dad447f8a"
-    region         = "us-east-1"
+    
     tags = {
         Name = "Myinstance-Remote-State"
     }
